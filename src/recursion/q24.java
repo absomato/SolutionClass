68f892c3ad41d0b5eba70ace5aa19440386f2e3f
absomato <absomato@naver.com>
2018-11-13 17:54:09 +0900

    first version

--- src/recursion/q24.java
+++ src/recursion/q24.java
@@ -1,6 +1,6 @@
 package recursion;
 
-public class q24 {
+/*public class q24 {
 	static int sum=21; // 부분집합 내 정수들의 합이 되어야 할 값
 	static int n[]={1,3,4,5,8,9}; // 입력: n개 정수
 	public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class q24 {
 				if(subset[j]==1)s+=n[j];
 			if(s==sum) {
 				for(int j=0;j<n.length;j++) {
-					if(subset[j]==1)System.out.print(n[j]);
+					if(subset[j]==1)System.out.print(n[j]+" ");
 				}
 				System.out.println();
 			}
@@ -26,4 +26,38 @@ public class q24 {
 		subset[i]=1;
 		subsetSum(subset, i+1);
 	}
+}*/
+
+
+public class q24 {
+	static int sum=21; // 부분집합 내 정수들의 합이 되어야 할 값
+	static int n[]={1,3,4,5,8,9}; // 입력: n개 정수
+	public static void main(String[] args) {
+		int subset[]=new int[n.length];
+		subsetSum(subset, 0,0);
+	}
+	private static void subsetSum(int[] subset, int i,int partialSum) {
+		if(i==n.length){
+			if(partialSum==sum) {
+				for(int j=0;j<n.length;j++) {
+					if(subset[j]==1)System.out.print(n[j]+" ");
+				}
+				System.out.println();
+			}
+			// 부분집합 내 정수의 합이 sum이 되는지 검사 후 부분집합 출력
+			return;
+		}
+		subset[i]=0;
+		subsetSum(subset, i+1,partialSum);
+		subset[i]=1;
+		subsetSum(subset, i+1,partialSum+n[i]);
+	}
 }
+
+
+
+
+
+
+
+
---
+++ src/recursion/q27.java
@@ -0,0 +1,25 @@
+package recursion;
+
+/*public class q27 {
+	static int sum=21;
+	public static void main(String[] args) {
+		int n[]={1,3,4,5,8,9};
+		System.out.println(subsetSum(n, 0, 0));
+	}
+	private static boolean subsetSum(int[] n, int i, int partialSum) {
+		if(i==n.length) return partialSum==sum;
+		return subsetSum(n, i+1, partialSum) || subsetSum(n, i+1, partialSum+n[i]);
+	}
+}*/
+public class q27 { // 미완성 코드
+	static int sum=21;
+	public static void main(String[] args) {
+		int n[]={1,3,4,5,8,9};
+		System.out.println(subsetSum(n, 0, sum));
+	}
+	private static boolean subsetSum(int[] n, int i, int partialSum) {
+		if(i==n.length) return partialSum==0;
+		return subsetSum(n, i+1, partialSum) || subsetSum(n, i+1, partialSum-n[i]);
+
+	}
+}

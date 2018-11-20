3b019c7636f1b4da1247f674d1e8a9e0ae42d502
absomato <absomato@naver.com>
2018-11-20 17:50:25 +0900

    first version

--- src/recursion/q15.java
+++ src/recursion/q15.java
@@ -38,35 +38,45 @@ public class q15 {
 import java.util.Random;
 
 public class q15 {
-	static int cnt=1;
+	static int cnt = 1;
+
 	public static void main(String[] args) {
-		int R=5, C=5, count=0;
-		char ground[][]=new char[R][C];
-		Random random=new Random();
-		for (int i = 0; i < ground.length; i++){ // 임의 개수의 물웅덩이 포함 평면 생성
-			for (int j = 0; j < ground[i].length; j++) ground[i][j]=(random.nextInt(3)==0)? '1' : '0';
+		int R = 5, C = 5;
+		char ground[][] = new char[R][C];
+		Random random = new Random();
+		for (int i = 0; i < ground.length; i++) { // 임의 개수의 물웅덩이 포함 평면 생성
+			for (int j = 0; j < ground[i].length; j++)
+				ground[i][j] = (random.nextInt(3) == 0) ? '1' : '0';
 		}
-		for (int i = 0; i < ground.length; i++){ // 평면 출력
-			for (int j = 0; j < ground[i].length; j++) System.out.print(ground[i][j]);
+		for (int i = 0; i < ground.length; i++) { // 평면 출력
+			for (int j = 0; j < ground[i].length; j++)
+				System.out.print(ground[i][j]);
 			System.out.println();
 		}
 		for (int i = 0; i < ground.length; i++) {
-			for (int j = 0; j < ground[i].length; j++){
-				if(ground[i][j]=='1'){ground[i][j]=0; dfs(ground, R, C, i, j);
-				System.out.print(cnt+" ");cnt=1;}
+			for (int j = 0; j < ground[i].length; j++) {
+				if (ground[i][j] == '1') {
+					ground[i][j] = 0;
+					dfs(ground, R, C, i, j);
+					System.out.print(cnt + " ");
+					cnt = 1;
+				}
 			}
 		}
 	}
-	private static void dfs(char[][] ground, int R, int C, int i, int j) {
-		
-		int dx[]= {-1,-1,-1,0,0,1,1,1};
-		int dy[]= {-1,0,1,-1,1,-1,0,1};
-		for(int k=0;k<dx.length;k++) {
 
-			if(i+dx[k]<0 || i+dx[k]>=R || j+dy[k]<0 || j+dy[k]>=C)continue;
-			if(ground[i+dx[k]][j+dy[k]]=='1') {cnt++;
-				ground[i+dx[k]][j+dy[k]]='0';
-				dfs(ground,R,C,i+dx[k],j+dy[k]);
+	private static void dfs(char[][] ground, int R, int C, int i, int j) {
+
+		int dx[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+		int dy[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+		for (int k = 0; k < dx.length; k++) {
+
+			if (i + dx[k] < 0 || i + dx[k] >= R || j + dy[k] < 0 || j + dy[k] >= C)
+				continue;
+			if (ground[i + dx[k]][j + dy[k]] == '1') {
+				cnt++;
+				ground[i + dx[k]][j + dy[k]] = '0';
+				dfs(ground, R, C, i + dx[k], j + dy[k]);
 			}
 		}
 	}
---
+++ src/sorting/q04.java
@@ -0,0 +1,70 @@
+package sorting;
+
+/*import java.util.Arrays;
+
+public class q04 {
+	public static void main(String[] args) {
+		int n[] = { 1, 3, 5, 7, 8 };
+		int m[] = { 2, 4, 6, 7, 10 };
+		int v[] = mergeArray(n, m);
+		System.out.println(Arrays.toString(v));
+	}
+
+	private static int[] mergeArray(int[] n, int[] m) {
+		int v[]=new int[n.length+m.length];
+		int ni = 0, mi = 0, k = 0;
+		while (true) {
+			if (ni < n.length && mi < m.length) {
+				if (n[ni] > m[mi])
+					v[k++] = m[mi++];
+				else
+					v[k++] = n[ni++];
+			} else {
+				if (ni >= n.length) {
+					for (int i = mi; i < m.length; i++) {
+						v[k++] = m[i];
+					}
+				} else {
+					for (int i = ni; i < n.length; i++) {
+						v[k++] = n[i];
+					}
+				}
+				break;
+			}
+		}
+		return v;
+	}
+}*/
+import java.util.Arrays;
+
+public class q04 {
+	public static void main(String[] args) {
+		int n[] = { 1, 3, 5, 7, 8 };
+		int m[] = { 2, 4, 6, 7, 10 };
+		int v[] = mergeArray(n, m);
+		System.out.println(Arrays.toString(v));
+	}
+
+	private static int[] mergeArray(int[] n, int[] m) {
+		int v[] = new int[n.length + m.length];
+		int ni = 0, mi = 0, k = 0;
+		while (ni < n.length && mi < m.length) {
+
+			if (n[ni] > m[mi])
+				v[k++] = m[mi++];
+			else
+				v[k++] = n[ni++];
+		}
+		if (ni >= n.length) {
+			for (int i = mi; i < m.length; i++) {
+				v[k++] = m[i];
+			}
+		} else {
+			for (int i = ni; i < n.length; i++) {
+				v[k++] = n[i];
+			}
+		}
+
+		return v;
+	}
+}
